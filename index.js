fb51c1f39a2e1eba312d92c6ff95b82516c16551
Will <willianchua.wc@gmail.com>
2024-01-23 14:04:50 +0800

    Add Grid Scaling Feature
    After debugging for an hour, exploring documentation and flex properties, the grid can have MxN cells without exceeding the max width.
    
    I learned the power of flex grow and its impact on empty containers. It was troublesome trying to find a percentage version of padding. I knew that this was possible, even if I did not know how. In the end, I figured it out.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const DUMMY_TEXT = "DUMMY TEXT";
 
 const body = document.querySelector("body");
-body.style.maxWidth = "960px";
-body.style.maxHeight = "960px";
-body.style.display = "block";
+body.style.display = "flex";
+body.style.flexDirection = "column";
+body.style.alignItems = "center";
 body.style.margin = "0 auto";
+
+
 const button = document.createElement("button");
 button.textContent = "Initialize Grid";
-button.style.padding = "10px";
-button.style.display = "block";
-button.style.margin = "10px auto";
+button.style.fontSize = "1em";
+button.style.padding = "20px";
+button.style.margin = "20px";
 body.appendChild(button);
 
 let gridContainer = document.createElement("div");
@@ -17,15 +19,17 @@ body.appendChild(gridContainer);
 
 let length = 16;
 let width = 16;
+let itemSize = 960 / (length * width);
 button.addEventListener("click", () => {
-    length = prompt("Input length of grid (X axis)");
+    length = prompt("Input number of columns (X axis)");
     while (length > 100 || length < 1){
-        length = prompt("Length of grid must be within 1-100");
+        length = prompt("Number of columns must be within 1-100");
     }
-    width = prompt("Input width of grid (Y axis)");
+    width = prompt("Input number of rows (Y axis)");
     while (width > 100 || width < 1){
-        width = prompt("Width of grid must be within 1-100");
+        width = prompt("Number of rows must be within 1-100");
     }
+    initializeItemSize();
     generateGrid();
 })
 
@@ -38,10 +42,14 @@ function generateGrid(){
     for (let i = 0; i < width; i++){
         rowContainers[i] = document.createElement("div");
         rowContainers[i].style.cssText = "display: flex; flex-direction: row; background-color: blue;";
+        rowContainers[i].style.flexGrow = 1;
         rowItems[i] = [length];
+
         for (let j = 0; j < length; j++){
             rowItems[i][j] = document.createElement("div");
-            rowItems[i][j].style.cssText = "background-color: yellow; padding: 10px;";
+            rowItems[i][j].style.flexGrow = 1;
+
+            rowItems[i][j].style.backgroundColor = "yellow";
             rowItems[i][j].addEventListener("mouseenter", (e) => {
                 e.currentTarget.style.backgroundColor = "black";
             })
@@ -56,9 +64,16 @@ function generateGrid(){
 function initializeGridContainer(){
     gridContainer = document.createElement("div");
     gridContainer.setAttribute("id", "gridContainer");
-    gridContainer.style.cssText = "display: flex; flex-direction: column; background-color: red; align-content: stretch;";
+    gridContainer.style.cssText = "display: flex; flex-direction: column; background-color: red;";
+    gridContainer.style.maxWidth = "960px";
+    gridContainer.style.minWidth = "960px";
+    gridContainer.style.minHeight = "780px";
+    gridContainer.style.maxHeight = "780px";
 }
 
+function initializeItemSize(){
+    itemSize = 960 / (width * length);
+}
 generateGrid();
 
  
